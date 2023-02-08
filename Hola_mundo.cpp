928d71c4f7e9fdad9ca1bcfe29c32270094b69c0
Hanner <hannerchuc@gmail.com>
2023-02-08 08:34:33 -0600

    Comit inicial

---
+++ Hola_mundo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+// siempre usar estandar de c++, el programa pone el std::<<cout
+using namespace std;
+// inicio de bucle del programa
+int main()
+{
+  cout << "HOLA MUNDO";
+  // buena practica de programación para saber que el programa se ejecuto exitosamente
+  return 0;
+}
